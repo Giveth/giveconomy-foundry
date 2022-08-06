98bd938efc65c128ba65f80ac5d1078451ec40f2
0xGabi <gabrielpk.18@gmail.com>
2022-08-06 01:54:20 -0300

    test: keep rewriting test in foundry

--- test/GIVpower.t.sol
+++ test/GIVpower.t.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity =0.8.6;
 
-import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
-import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
-import "forge-std/Test.sol";
-import "forge-std/console.sol";
+import '@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol';
+import '@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol';
+import 'forge-std/Test.sol';
+import 'forge-std/console.sol';
 
-import "contracts/GIVpower.sol";
-import "contracts/GardenUnipoolTokenDistributor.sol";
-import "./interfaces/IERC20Bridged.sol";
+import 'contracts/GIVpower.sol';
+import 'contracts/GardenUnipoolTokenDistributor.sol';
+import './interfaces/IERC20Bridged.sol';
 
 library Math {
     function sqrt(uint256 y) internal pure returns (uint256 z) {
@@ -35,14 +35,14 @@ contract GIVpowerTest is Test {
     IERC20Bridged givToken;
     address givethMultisig;
 
+    // constants
+    uint256 constant sqrtPrecision = 10e9;
+
     // accounts
     address sender = address(1);
     address notAuthorized = address(2);
     address omniBridge = 0xf6A78083ca3e2a662D6dd1703c939c8aCE2e268d;
-    address[] testUsers = [
-        0xB8306b6d3BA7BaB841C02f0F92b8880a4760199A,
-        0x975f6807E8406191D1C951331eEa4B26199b37ff
-    ];
+    address[] testUsers = [0xB8306b6d3BA7BaB841C02f0F92b8880a4760199A, 0x975f6807E8406191D1C951331eEa4B26199b37ff];
 
     struct StorageData {
         address tokenManager;
@@ -62,13 +62,9 @@ contract GIVpowerTest is Test {
     StorageData storageDataBeforeUpgrade;
 
     function setUp() public {
-        gardenUnipoolProxyAdmin = ProxyAdmin(
-            address(0x076C250700D210e6cf8A27D1EB1Fd754FB487986)
-        );
+        gardenUnipoolProxyAdmin = ProxyAdmin(address(0x076C250700D210e6cf8A27D1EB1Fd754FB487986));
 
-        gardenUnipoolProxy = TransparentUpgradeableProxy(
-            payable(0xD93d3bDBa18ebcB3317a57119ea44ed2Cf41C2F2)
-        );
+        gardenUnipoolProxy = TransparentUpgradeableProxy(payable(0xD93d3bDBa18ebcB3317a57119ea44ed2Cf41C2F2));
 
         // new implementation
         implementation = new GIVpower();
@@ -86,29 +82,23 @@ contract GIVpowerTest is Test {
 
         // upgrade to new implementation
         vm.prank(givethMultisig);
-        gardenUnipoolProxyAdmin.upgrade(
-            gardenUnipoolProxy,
-            address(implementation)
-        );
+        gardenUnipoolProxyAdmin.upgrade(gardenUnipoolProxy, address(implementation));
 
         // mint
         vm.prank(omniBridge);
         givToken.mint(sender, 100 ether);
 
         // labels
-        vm.label(notAuthorized, "notAuthorizedAddress");
-        vm.label(givethMultisig, "givethMultisig");
-        vm.label(address(gardenUnipoolProxyAdmin), "ProxyAdmin");
-        vm.label(address(gardenUnipoolProxy), "Proxy");
-        vm.label(address(givPower), "GIVpower");
-        vm.label(address(tokenManager), "TokenManager");
-        vm.label(address(givToken), "GivethToken");
+        vm.label(notAuthorized, 'notAuthorizedAddress');
+        vm.label(givethMultisig, 'givethMultisig');
+        vm.label(address(gardenUnipoolProxyAdmin), 'ProxyAdmin');
+        vm.label(address(gardenUnipoolProxy), 'Proxy');
+        vm.label(address(givPower), 'GIVpower');
+        vm.label(address(tokenManager), 'TokenManager');
+        vm.label(address(givToken), 'GivethToken');
     }
 
-    function getImplementationStorageData(address[] memory _users)
-        public
-        returns (StorageData memory)
-    {
+    function getImplementationStorageData(address[] memory _users) public returns (StorageData memory) {
         uint256[] memory usersBalances = new uint256[](_users.length);
         uint256[] memory usersRewardsPerTokenPaid = new uint256[](
             _users.length
@@ -117,27 +107,24 @@ contract GIVpowerTest is Test {
 
         for (uint256 i = 0; i < _users.length; i++) {
             usersBalances[i] = givPower.balanceOf(_users[i]);
-            usersRewardsPerTokenPaid[i] = givPower.userRewardPerTokenPaid(
-                _users[i]
-            );
+            usersRewardsPerTokenPaid[i] = givPower.userRewardPerTokenPaid(_users[i]);
             usersRewards[i] = givPower.rewards(_users[i]);
         }
 
-        return
-            StorageData({
-                tokenManager: givPower.getTokenManager(),
-                tokenDistro: address(givPower.tokenDistro()),
-                duration: givPower.duration(),
-                rewardDistribution: givPower.rewardDistribution(),
-                periodFinish: givPower.periodFinish(),
-                rewardRate: givPower.rewardRate(),
-                lastUpdateTime: givPower.lastUpdateTime(),
-                rewardPerTokenStored: givPower.rewardPerTokenStored(),
-                totalSupply: givPower.totalSupply(),
-                usersBalances: usersBalances,
-                usersRewardsPerTokenPaid: usersRewardsPerTokenPaid,
-                usersRewards: usersRewards
-            });
+        return StorageData({
+            tokenManager: givPower.getTokenManager(),
+            tokenDistro: address(givPower.tokenDistro()),
+            duration: givPower.duration(),
+            rewardDistribution: givPower.rewardDistribution(),
+            periodFinish: givPower.periodFinish(),
+            rewardRate: givPower.rewardRate(),
+            lastUpdateTime: givPower.lastUpdateTime(),
+            rewardPerTokenStored: givPower.rewardPerTokenStored(),
+            totalSupply: givPower.totalSupply(),
+            usersBalances: usersBalances,
+            usersRewardsPerTokenPaid: usersRewardsPerTokenPaid,
+            usersRewards: usersRewards
+        });
     }
 
     function roundHasStartedInSeconds() public returns (uint256) {
@@ -145,94 +132,37 @@ contract GIVpowerTest is Test {
     }
 
     function testImplementationStorage() public {
-        StorageData
-            memory storageDataAfterUpgrade = getImplementationStorageData(
-                testUsers
-            );
+        StorageData memory storageDataAfterUpgrade = getImplementationStorageData(testUsers);
 
         assertEq(givPower.roundDuration(), 14 days);
         assertEq(givPower.maxLockRounds(), 26);
 
-        assertEq(
-            storageDataBeforeUpgrade.tokenManager,
-            storageDataAfterUpgrade.tokenManager
-        );
-        assertEq(
-            storageDataBeforeUpgrade.tokenDistro,
-            storageDataAfterUpgrade.tokenDistro
-        );
-        assertEq(
-            storageDataBeforeUpgrade.duration,
-            storageDataAfterUpgrade.duration
-        );
-        assertEq(
-            storageDataBeforeUpgrade.rewardDistribution,
-            storageDataAfterUpgrade.rewardDistribution
-        );
-        assertEq(
-            storageDataBeforeUpgrade.periodFinish,
-            storageDataAfterUpgrade.periodFinish
-        );
-        assertEq(
-            storageDataBeforeUpgrade.rewardRate,
-            storageDataAfterUpgrade.rewardRate
-        );
-        assertEq(
-            storageDataBeforeUpgrade.lastUpdateTime,
-            storageDataAfterUpgrade.lastUpdateTime
-        );
-        assertEq(
-            storageDataBeforeUpgrade.rewardPerTokenStored,
-            storageDataAfterUpgrade.rewardPerTokenStored
-        );
-        assertEq(
-            storageDataBeforeUpgrade.totalSupply,
-            storageDataAfterUpgrade.totalSupply
-        );
+        assertEq(storageDataBeforeUpgrade.tokenManager, storageDataAfterUpgrade.tokenManager);
+        assertEq(storageDataBeforeUpgrade.tokenDistro, storageDataAfterUpgrade.tokenDistro);
+        assertEq(storageDataBeforeUpgrade.duration, storageDataAfterUpgrade.duration);
+        assertEq(storageDataBeforeUpgrade.rewardDistribution, storageDataAfterUpgrade.rewardDistribution);
+        assertEq(storageDataBeforeUpgrade.periodFinish, storageDataAfterUpgrade.periodFinish);
+        assertEq(storageDataBeforeUpgrade.rewardRate, storageDataAfterUpgrade.rewardRate);
+        assertEq(storageDataBeforeUpgrade.lastUpdateTime, storageDataAfterUpgrade.lastUpdateTime);
+        assertEq(storageDataBeforeUpgrade.rewardPerTokenStored, storageDataAfterUpgrade.rewardPerTokenStored);
+        assertEq(storageDataBeforeUpgrade.totalSupply, storageDataAfterUpgrade.totalSupply);
 
-        for (
-            uint256 i = 0;
-            i < storageDataBeforeUpgrade.usersBalances.length;
-            i++
-        ) {
+        for (uint256 i = 0; i < storageDataBeforeUpgrade.usersBalances.length; i++) {
+            assertEq(storageDataBeforeUpgrade.usersBalances[i], storageDataAfterUpgrade.usersBalances[i]);
             assertEq(
-                storageDataBeforeUpgrade.usersBalances[i],
-                storageDataAfterUpgrade.usersBalances[i]
+                storageDataBeforeUpgrade.usersRewardsPerTokenPaid[i], storageDataAfterUpgrade.usersRewardsPerTokenPaid[i]
             );
-            assertEq(
-                storageDataBeforeUpgrade.usersRewardsPerTokenPaid[i],
-                storageDataAfterUpgrade.usersRewardsPerTokenPaid[i]
-            );
-            assertEq(
-                storageDataBeforeUpgrade.usersRewards[i],
-                storageDataAfterUpgrade.usersRewards[i]
-            );
+            assertEq(storageDataBeforeUpgrade.usersRewards[i], storageDataAfterUpgrade.usersRewards[i]);
         }
     }
 
     function testWrap() public {
-        uint256 lockAmount = 100 ether;
-        uint256 numberOfRounds = 1;
-        uint256 initialTotalSupply = givPower.totalSupply();
-
-        uint256 powerIncreaseAfterLockExpected = (lockAmount *
-            Math.sqrt(1 + numberOfRounds)) - lockAmount;
-
-        uint256 powerIncreaseAfterLock = givPower.calculatePower(
-            lockAmount,
-            numberOfRounds
-        ) - lockAmount;
-
-        assertApproxEqRel(
-            powerIncreaseAfterLockExpected,
-            powerIncreaseAfterLock,
-            20
-        );
+        // TBD
     }
 
     function testIsNonTransferableToken() public {
-        assertEq(givPower.name(), "GIVpower");
-        assertEq(givPower.symbol(), "POW");
+        assertEq(givPower.name(), 'GIVpower');
+        assertEq(givPower.symbol(), 'POW');
         assertEq(givPower.decimals(), 18);
         assertEq(givPower.balanceOf(address(0)), 0);
         assertFalse(givPower.totalSupply() == 0);
