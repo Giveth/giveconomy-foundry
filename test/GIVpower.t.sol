67526cfdd7b0ce3b6c5cfafd42896d1cb8884603
Amin Latifi <a.latifi.al@gmail.com>
2022-08-07 20:35:43 +0430

    Added fork xdai chain config to GIVpower.t.sol test Fixed testForbidUnwrapTokensWhileLock test issue due to different time config in foundry test env

--- test/GIVpower.t.sol
+++ test/GIVpower.t.sol
@@ -66,6 +66,9 @@ contract GIVpowerTest is Test {
     event TokenUnlocked(address indexed account, uint256 amount, uint256 round);
 
     function setUp() public {
+        uint256 forkId = vm.createFork('https://xdai-archive.blockscout.com/', 22501098);
+        vm.selectFork(forkId);
+
         gardenUnipoolProxyAdmin = ProxyAdmin(address(0x076C250700D210e6cf8A27D1EB1Fd754FB487986));
 
         gardenUnipoolProxy = TransparentUpgradeableProxy(payable(0xD93d3bDBa18ebcB3317a57119ea44ed2Cf41C2F2));
@@ -359,6 +362,8 @@ contract GIVpowerTest is Test {
 
         vm.expectRevert(GIVpower.CannotUnlockUntilRoundIsFinished.selector);
         givPower.unlock(accounts, round);
+
+        skip(1);
 
         uint256 passedSeconds = roundHasStartedInSeconds();
         // Seconds has passed from round start time, for this test it should be positive
