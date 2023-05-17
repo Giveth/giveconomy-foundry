05a885e85ccdcb5280b417f8d45cb35a07c395d5
Mitch Oz <divine_comedian@protonmail.com>
2023-05-17 12:08:14 -0600

    modify garden unipool - remove tokenmanager

--- contracts/ModifiedUnipoolTokenDistributor.sol
+++ contracts/ModifiedUnipoolTokenDistributor.sol
@@ -1,5 +1,15 @@
+/**
+ * Contract has the most of the functionalities of UnipoolTokenDistributor contract, but is updated
+ * to be compatible with token-manager-app of 1Hive.
+ * 1. Stake/Withdraw methods are updated to internal type.
+ * 2. Methods related to permit are removed.
+ * 3. Stake/Withdraw are update based on 1Hive unipool (https://github.com/1Hive/unipool/blob/master/contracts/Unipool.sol).
+ * This PR was the guide: https://github.com/1Hive/unipool/pull/7/files
+ */
+
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.6;
+
+pragma solidity =0.8.6;
 
 import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/utils/math/MathUpgradeable.sol';
@@ -23,9 +33,10 @@ contract LPTokenWrapper is Initializable {
     using SafeMathUpgradeable for uint256;
     using SafeERC20Upgradeable for IERC20Upgradeable;
 
+
     IERC20Upgradeable public uni;
 
-    uint256 private _totalSupply;
+    uint256 private _totalStaked;
     mapping(address => uint256) private _balances;
 
     function __LPTokenWrapper_initialize(IERC20Upgradeable _uni)
@@ -34,32 +45,38 @@ contract LPTokenWrapper is Initializable {
     {
         uni = _uni;
     }
-
-    function totalSupply() public view returns (uint256) {
-        return _totalSupply;
+    // Should we make these public functions?
+    function _totalSupply() internal view returns (uint256) {
+        return _totalStaked;
     }
 
-    function balanceOf(address account) public view returns (uint256) {
+    // Should we make these public functions?
+    function _balanceOf(address account) internal view returns (uint256) {
         return _balances[account];
     }
+        // Should we make these public functions?
 
-    function stake(uint256 amount) public virtual {
-        _totalSupply = _totalSupply.add(amount);
-        _balances[msg.sender] = _balances[msg.sender].add(amount);
+    function stake(address user, uint256 amount) internal virtual {
+        _totalStaked = _totalStaked.add(amount);
+        _balances[user] = _balances[user].add(amount);
         uni.safeTransferFrom(msg.sender, address(this), amount);
-    }
 
-    function withdraw(uint256 amount) public virtual {
-        _totalSupply = _totalSupply.sub(amount);
-        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+    }
+        // Should we make these public functions?
+
+    function withdraw(address user, uint256 amount) internal virtual {
+        _totalStaked = _totalStaked.sub(amount);
+        _balances[user] = _balances[user].sub(amount);
         uni.safeTransfer(msg.sender, amount);
+
     }
 }
 
-contract UnipoolTokenDistributor is LPTokenWrapper, OwnableUpgradeable {
+contract ModifiedUnipoolTokenDistributor is LPTokenWrapper, OwnableUpgradeable {
     using SafeMathUpgradeable for uint256;
     using SafeERC20Upgradeable for IERC20Upgradeable;
 
+    
     IDistro public tokenDistro;
     uint256 public duration;
 
@@ -76,7 +93,7 @@ contract UnipoolTokenDistributor is LPTokenWrapper, OwnableUpgradeable {
     event Withdrawn(address indexed user, uint256 amount);
     event RewardPaid(address indexed user, uint256 reward);
 
-    // bytes4 private constant _PERMIT_SIGNATURE =
+        // bytes4 private constant _PERMIT_SIGNATURE =
     //    bytes4(keccak256(bytes("permit(address,address,uint256,uint256,uint8,bytes32,bytes32)")));
     bytes4 private constant _PERMIT_SIGNATURE = 0xd505accf;
     // bytes4 private constant _PERMIT_SIGNATURE_BRIDGE =
@@ -84,10 +101,7 @@ contract UnipoolTokenDistributor is LPTokenWrapper, OwnableUpgradeable {
     bytes4 private constant _PERMIT_SIGNATURE_BRIDGE = 0x8fcbaf0c;
 
     modifier onlyRewardDistribution() {
-        require(
-            _msgSender() == rewardDistribution,
-            "Caller is not reward distribution"
-        );
+        require(_msgSender() == rewardDistribution, 'Caller is not reward distribution');
         _;
     }
 
@@ -95,7 +109,7 @@ contract UnipoolTokenDistributor is LPTokenWrapper, OwnableUpgradeable {
         rewardPerTokenStored = rewardPerToken();
         lastUpdateTime = lastTimeRewardApplicable();
         if (account != address(0)) {
-            rewards[account] = earned(account);
+            rewards[account] = claimableStream(account);
             userRewardPerTokenPaid[account] = rewardPerTokenStored;
         }
         _;
@@ -105,7 +119,8 @@ contract UnipoolTokenDistributor is LPTokenWrapper, OwnableUpgradeable {
         IDistro _tokenDistribution,
         IERC20Upgradeable _uni,
         uint256 _duration
-    ) public initializer {
+      ) public initializer {
+        
         __Ownable_init();
         __LPTokenWrapper_initialize(_uni);
         tokenDistro = _tokenDistribution;
@@ -115,56 +130,76 @@ contract UnipoolTokenDistributor is LPTokenWrapper, OwnableUpgradeable {
     }
 
     function lastTimeRewardApplicable() public view returns (uint256) {
-        return MathUpgradeable.min(_getBlockTimestamp(), periodFinish);
+        return MathUpgradeable.min(getTimestamp(), periodFinish);
     }
 
     function rewardPerToken() public view returns (uint256) {
-        if (totalSupply() == 0) {
+        if (_totalSupply() == 0) {
             return rewardPerTokenStored;
         }
-        return
-            rewardPerTokenStored.add(
-                lastTimeRewardApplicable()
-                    .sub(lastUpdateTime)
-                    .mul(rewardRate)
-                    .mul(1e18)
-                    .div(totalSupply())
-            );
+        return rewardPerTokenStored.add(
+            lastTimeRewardApplicable()
+            .sub(lastUpdateTime)
+            .mul(rewardRate)
+            .mul(1e18)
+            .div(_totalSupply())
+        );
     }
 
-    function earned(address account) public view returns (uint256) {
-        return
-            balanceOf(account)
-                .mul(rewardPerToken().sub(userRewardPerTokenPaid[account]))
-                .div(1e18)
-                .add(rewards[account]);
+    /**
+     * Function to get the current timestamp from the block
+     */
+    function getTimestamp() public view virtual returns (uint256) {
+        return block.timestamp;
     }
 
-    // stake visibility is public as overriding LPTokenWrapper's stake() function
-    function stake(uint256 amount) public override updateReward(msg.sender) {
-        require(amount > 0, "Cannot stake 0");
-        super.stake(amount);
-        emit Staked(msg.sender, amount);
+    /**
+     * Function to get the amount of tokens is transferred in the claim tx
+     * @notice The difference between what this returns and what the claimableStream function returns
+     * will be locked in TokenDistro to be streamed and released gradually
+     */
+    function earned(address account) external view returns (uint256) {
+        uint256 _totalEarned = claimableStream(account);
+        uint256 _tokenDistroReleasedTokens = tokenDistro.globallyClaimableAt(getTimestamp());
+        uint256 _tokenDistroTotalTokens = tokenDistro.totalTokens();
+
+        return (_totalEarned * _tokenDistroReleasedTokens) / _tokenDistroTotalTokens;
     }
 
-    function withdraw(uint256 amount) public override updateReward(msg.sender) {
-        require(amount > 0, "Cannot withdraw 0");
-        super.withdraw(amount);
-        emit Withdrawn(msg.sender, amount);
+    // @dev This does the same thing the earned function of UnipoolTokenDistributor contract does.
+    // Returns the exact amount will be allocated on TokenDistro
+    function claimableStream(address account) public view returns (uint256) {
+        return _balanceOf(account).mul(rewardPerToken().sub(userRewardPerTokenPaid[account])).div(1e18).add(
+            rewards[account]
+        );
     }
 
-    function exit() external {
-        withdraw(balanceOf(msg.sender));
-        getReward();
+    function stake(address user, uint256 amount) internal override updateReward(user) {
+        require(amount > 0, 'Cannot stake 0');
+        super.stake(user, amount);
+        emit Staked(user, amount);
+    }
+
+    function withdraw(address user, uint256 amount) internal override updateReward(user) {
+        require(amount > 0, 'Cannot withdraw 0');
+        super.withdraw(user, amount);
+        if (_balanceOf(user) == 0) {
+            _getReward(user);
+        }
+        emit Withdrawn(user, amount);
     }
 
     function getReward() public updateReward(msg.sender) {
-        uint256 reward = earned(msg.sender);
+        _getReward(msg.sender);
+    }
+
+    function _getReward(address user) internal {
+        uint256 reward = claimableStream(user);
         if (reward > 0) {
-            rewards[msg.sender] = 0;
+            rewards[user] = 0;
             //token.safeTransfer(msg.sender, reward);
-            tokenDistro.allocate(msg.sender, reward, true);
-            emit RewardPaid(msg.sender, reward);
+            tokenDistro.allocate(user, reward, true);
+            emit RewardPaid(user, reward);
         }
     }
 
@@ -173,7 +208,7 @@ contract UnipoolTokenDistributor is LPTokenWrapper, OwnableUpgradeable {
         onlyRewardDistribution
         updateReward(address(0))
     {
-        uint256 _timestamp = _getBlockTimestamp();
+        uint256 _timestamp = getTimestamp();
         if (_timestamp >= periodFinish) {
             rewardRate = reward.div(duration);
         } else {
@@ -186,10 +221,7 @@ contract UnipoolTokenDistributor is LPTokenWrapper, OwnableUpgradeable {
         emit RewardAdded(reward);
     }
 
-    function setRewardDistribution(address _rewardDistribution)
-        external
-        onlyOwner
-    {
+    function setRewardDistribution(address _rewardDistribution) external onlyOwner {
         rewardDistribution = _rewardDistribution;
     }
 
@@ -198,7 +230,7 @@ contract UnipoolTokenDistributor is LPTokenWrapper, OwnableUpgradeable {
      * @param amount the amount to be staked, it has to match the amount that appears in the signature
      * @param permit the bytes of the signed permit function call
      */
-    function stakeWithPermit(uint256 amount, bytes calldata permit)
+    function stakeWithPermit(address user, uint256 amount, bytes calldata permit)
         public
         updateReward(msg.sender)
     {
@@ -207,7 +239,7 @@ contract UnipoolTokenDistributor is LPTokenWrapper, OwnableUpgradeable {
         // the following transferFrom should be fail. This prevents DoS attacks from using a signature
         // before the smartcontract call
         _permit(amount, permit);
-        super.stake(amount);
+        super.stake(user, amount);
         emit Staked(msg.sender, amount);
     }
 
@@ -328,11 +360,4 @@ contract UnipoolTokenDistributor is LPTokenWrapper, OwnableUpgradeable {
         }
     }
 
-    /// @dev Internal method that returns the current block timestamp
-    /// We expect this function call to be optimized away
-    /// Mock implementations can override this to set a fixed block timestamp value
-    /// @return The current block timestamp
-    function _getBlockTimestamp() internal view virtual returns (uint256) {
-        return block.timestamp;
-    }
 }
